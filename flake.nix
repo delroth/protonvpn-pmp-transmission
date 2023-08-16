6275d44bbf12dfd39b3556dab8dbf812387fc154
Pierre Bourdon <delroth@gmail.com>
2023-08-16 08:34:11 +0200

    flake: init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A daemon to allocate ProtonVPN port mappings and inform Transmission of said mappings.";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
+    overlay = nixpkgs.lib.composeManyExtensions [
+      poetry2nix.overlay
+      (final: prev: {
+        protonvpn-pmp-transmission = prev.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+        };
+      })
+    ];
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in rec {
+      packages.protonvpn-pmp-transmission = pkgs.protonvpn-pmp-transmission;
+      defaultPackage = pkgs.protonvpn-pmp-transmission;
+
+      devShells.default = with pkgs; mkShell {
+        buildInputs = [ poetry ];
+      };
+    }
+  ));
+}
